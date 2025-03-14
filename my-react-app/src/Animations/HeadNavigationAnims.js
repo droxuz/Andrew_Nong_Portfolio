6923e82ecf12b60e1167a1899e60aef7e56861d2
droxuz <156976891+droxuz@users.noreply.github.com>
2025-03-14 18:49:25 -0400

    Added Framer Motion, Added animations to HeadNavigation

---
+++ my-react-app/src/Animations/HeadNavigationAnims.js
@@ -0,0 +1,6 @@
+import {motion} from "framer-motion";
+
+export const fadeInfromTop = {
+    initial: { y: -50, opacity: 0 }, // Start 50px above and invisible
+    animate: { y: 0, opacity: 1 }, // Move to original position and fade in
+  };
--- my-react-app/src/Components/HeadNavigation/HeadNavigation.jsx
+++ my-react-app/src/Components/HeadNavigation/HeadNavigation.jsx
@@ -1,13 +1,58 @@
+import { fadeInfromTop } from "../../Animations/HeadNavigationAnims";
 import "./HeadNavigation.css";
+import {motion} from 'framer-motion';
 
 function HeadNavigation(){
     return(
         <nav class="naviBar">
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Contact</a></li>
+                <li>
+                    <motion.a
+                    initial="initial"
+                    animate="animate"
+                    transition= {{ delay: 0.5, duration: 0.7}}
+                    variants={fadeInfromTop}
+                    href=""
+                    >
+                        Home
+                </motion.a></li>
+
+                <li>
+                    <motion.a
+                    initial="initial"
+                    animate="animate"
+                    transition= {{ delay: 0.5, duration: 0.9}}
+                    variants={fadeInfromTop}
+                    href=""
+                    >
+                        About
+                    </motion.a>
+                </li>
+
+                <li>
+                    <motion.a
+                    initial="initial"
+                    animate="animate"
+                    transition= {{ delay: 0.5, duration: 1.1}}
+                    variants={fadeInfromTop}
+                    href=""
+                    >
+                        Projects
+                    </motion.a>
+                </li>
+
+                <li>
+                    <motion.a
+                    initial="initial"
+                    animate="animate"
+                    transition= {{ delay: 0.5, duration: 1.3}}
+                    variants={fadeInfromTop}
+                    href=""
+                    >
+                        Contact
+                    </motion.a>
+                </li>
+                
             </ul>
         </nav>
     );
