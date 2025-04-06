0b5ecfb11e94f36e5cb84b63327b9adcf18d9613
droxuz <156976891+droxuz@users.noreply.github.com>
2025-04-06 01:40:21 -0400

    uh

--- my-react-app/src/Components/LandingPage/LandingPage.jsx
+++ my-react-app/src/Components/LandingPage/LandingPage.jsx
@@ -1,39 +1,50 @@
 import './LandingPage.css'
-
+import {motion} from 'framer-motion'
 function LandingPage(){
     return(
-        <div class="LandingContainer">
-            <div class="WelcomeMSG">
+        <div className="LandingContainer">
+            <div className="WelcomeMSG">
             <h1>Welcome</h1>
             <p>I am Andrew and this is my portfolio</p>
             </div>
             
-            <div class="NavigationBlock">
-                <ul class="NavigationList">
+            <div className="NavigationBlock">
+                <ul className="NavigationList">
 
                     <li>
-                        <a href="">
+                        <motion.a
+                        whileHover={{
+                            scale: 1.2,
+                            transition: {duration: 1},
+                            textShadow: "0px 0px 8px rgb(215, 160, 252)",}}
+                        href="">
                         ABOUT
-                        </a>
+                        </motion.a>
                     </li>
 
                     <li>
-                        <a href="">
+                        <motion.a 
+                        whileHover={{
+                            scale: 1.2,
+                            transition: {duration: 1},
+                            textShadow: "0px 0px 8px rgb(215, 160, 252)",}} 
+                        href="">
                         PROJECTS
-                        </a>
+                        </motion.a>
                     </li>
 
                     <li>
-                        <a href="">
+                        <motion.a 
+                        whileHover={{
+                            scale: 1.2,
+                            transition: {duration: 0.5},
+                            textShadow: "0px 0px 8px rgb(215, 160, 252)",}} 
+                        href="">
                         CONTACT
-                        </a>
+                        </motion.a>
                     </li>
 
-                    <li>
-                        <a href="">
-
-                        </a>
-                    </li>
+                    
                 </ul>
             </div>
         </div>
