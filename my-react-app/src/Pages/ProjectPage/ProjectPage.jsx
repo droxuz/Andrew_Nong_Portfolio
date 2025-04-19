9315fc8fec96587dad81b0e7292bc51bd1f5256a
droxuz <156976891+droxuz@users.noreply.github.com>
2025-04-19 02:05:52 -0400

    erm

--- my-react-app/src/Pages/ProjectPage/ProjectPage.jsx
+++ my-react-app/src/Pages/ProjectPage/ProjectPage.jsx
@@ -1,12 +1,18 @@
 import HeadNavigation from "../../Components/HeadNavigation/HeadNavigation";
 import "./ProjectPage.css"
-
+import darksign from '../../Elements/Darksign_29.webp';
 function ProjectPage(){
     return(
         <div>
             <HeadNavigation />
-            <h1>This is the Project Page</h1>
+            <div className="darksignDiv">
+                <img src={darksign} alt="Darksign Symbol" />
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 }
 export default ProjectPage;
+
