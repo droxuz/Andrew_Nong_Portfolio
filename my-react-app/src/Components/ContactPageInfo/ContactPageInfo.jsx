acf65d07cd525f01324e7e9500586bae2c7a597d
droxuz <156976891+droxuz@users.noreply.github.com>
2025-06-30 17:09:47 -0400

    Completed Contacts Page and Fixed sizing and alignment

--- my-react-app/src/Components/ContactPageInfo/ContactPageInfo.jsx
+++ my-react-app/src/Components/ContactPageInfo/ContactPageInfo.jsx
@@ -1,4 +1,7 @@
 import './ContactPageInfo.css';
+import githubWhiteSVG from '../../Elements/github-mark-white.svg';
+import gmailFull from '../../Elements/google_gmail_icon.svg';
+import linkedinIcon from'../../Elements/linkedin-icon.svg';
 
 export default function ContactPageInfo(){
     return(
@@ -8,9 +11,24 @@ export default function ContactPageInfo(){
             </div>
             <div className="contactPageGridContainer">
                 <ul className="contactPageInfoGrid">
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>
+                    <a href="https://github.com/droxuz" target="_blank" rel="noopener" aria-label="GitHub">
+                        <img src={githubWhiteSVG} alt="githubWhiteSVG" />
+                        <span>GitHub</span>
+                    </a>
+                    </li>
+                    <li>
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=andrewnong54@gmail.com&su=Portfolio%20Inquiry" aria-label='Email'>
+                            <img src={gmailFull} alt="gmailFullColorSVG" />
+                            <span>Gmail</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/andrew-nong-4a6979305/" target="_blank" rel="noopener" aria-label="LinkedIn">
+                            <img src={linkedinIcon} alt="linkedinColorSVG" />
+                            <span>LinkedIn</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
